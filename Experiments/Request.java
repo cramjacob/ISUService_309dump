385472a233ac1b4c4691d0bf9ea0535d7e4bac8f
John Young <younjoh1@iastate.edu>
2018-09-09 00:59:04 -0500

    input vs output

---
+++ Experiments/Request.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Request{
+
+    
+    String name1;
+    int price1;
+
+    public Request(String name, int price){
+        name1 = name;
+        price1 = price;
+        getName();
+    }
+
+    public String getName(){
+        return name1;
+    }
+
+    public int getPrice(){
+        return price1;
+    }
+
+    public void setName(){
+        System.out.println("What's the request: ");
+        Scanner scan = new Scanner(System.in);
+        name1 = scan.next();
+    }
+
+
+    public static void main(String args[]){
+        Scanner scan2 = new Scanner(System.in);
+        Request r = new Request("Singing", 7);
+        System.out.println("Request Name: " + r.getName());
+        System.out.println("Price: " + r.getPrice());
+        System.out.println("Would you like to change request y/n");
+        String input = scan2.next();
+        if(input.equals("y")){
+            r.setName();
+            System.out.println();
+            System.out.println();
+            System.out.println("New request is: " + r.name1);
+        }
+        else{
+            System.out.println();
+            System.out.println();
+            System.out.println("Okay goodbye");
+        }
+    
+    }
+}
