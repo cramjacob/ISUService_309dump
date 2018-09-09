4285d4e606d771f87306dc1292ab1ec9e9b629e0
Daniel Laracuenta <dalc@iastate.edu>
2018-09-08 23:09:52 -0500

    Changed some things

--- Experiments/Hello309.java
+++ Experiments/Hello309.java
@@ -2,12 +2,11 @@ public class Hello309{
     
     public static void main(String[] args){
         System.out.println("Hello 309 World!");
-    }
 
-    public int RandomNumberGenerator{
-        int i = 0;
-        for(i; i < 20; i++){
+        int i;
+        for(i = 0; i <= 20; i++){
             System.out.println(i);
         }
+
     }
 }
