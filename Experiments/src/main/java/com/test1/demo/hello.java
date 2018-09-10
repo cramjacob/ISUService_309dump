31e48c0e75e293a26ac9287dce8dc933356ef309
John Young <younjoh1@iastate.edu>
2018-09-09 23:27:19 -0500

    taking input displaying array

--- Experiments/src/main/java/com/test1/demo/hello.java
+++ Experiments/src/main/java/com/test1/demo/hello.java
@@ -2,13 +2,21 @@ package com.test1.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Random;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class hello {
-
     @RequestMapping("/")
-    public String index() {
-        return "Simple display";
+    public int[] GetRandomNum(@RequestParam int input){
+        Random rand = new Random();
+        int[] num = new int[8];
+        for(int i = 0; i < num.length; i++){
+            num[i] = rand.nextInt(50);
+        }
+        if(input == 500){
+            return num;
+        }
+        return null;
     }
-
 }
