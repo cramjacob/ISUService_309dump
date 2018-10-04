0566f796607236bf512c32ae34627dc6fe6d353b
Sean <seansailer28@gmail.com>
2018-10-04 13:35:58 -0500

    Working backend, hallelujah

---
+++ Backend/Web/Models/Database/Offering.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Data;
+
+namespace Web.Models
+{
+    public class Offering
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public int UserID { get; set; }
+
+    }
+}
--- Backend/Web/Startup.cs
+++ Backend/Web/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Web.Data;
 
 namespace Web
 {
@@ -24,6 +26,10 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
